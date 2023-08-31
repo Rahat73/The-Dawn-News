3b0fae3fc3b5604b5e6c9290a98b390f8ed8b5dd
Rahat73 <rahat.ashik.18@gmail.com>
2023-09-01 02:52:06 +0600

    layout sortd, header updated

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,26 @@
 // import React from "react";
 
+import { Col, Container, Row } from "react-bootstrap";
 import Footer from "../pages/shared/Footer/Footer";
 import Header from "../pages/shared/Header/Header";
+import LeftNav from "../pages/shared/LeftNav/LeftNav";
+import RightNav from "../pages/shared/RightNav/RightNav";
 
 const Main = () => {
   return (
     <div>
       <Header></Header>
+      <Container>
+        <Row>
+          <Col lg={3}>
+            <LeftNav></LeftNav>
+          </Col>
+          <Col lg={6}>More contrnt coming soon...</Col>
+          <Col lg={3}>
+            <RightNav></RightNav>
+          </Col>
+        </Row>
+      </Container>
       <Footer></Footer>
     </div>
   );
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,7 +1,15 @@
 // import React from 'react';
+import moment from "moment/moment";
+import logo from "../../../assets/TDN-logo.png";
+import "./Header.css";
 
 const Header = () => {
-  return <div className="text-center">This is Header</div>;
+  return (
+    <div className="text-center tdn-logo">
+      <img src={logo} alt="tdn-logo" />
+      <p className="text-white pb-3">{moment().format("MMMM Do, YYYY")}</p>
+    </div>
+  );
 };
 
 export default Header;
